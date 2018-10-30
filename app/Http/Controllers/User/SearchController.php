55c47e6e2b89b8f062506a8a3cdf6f9ee752ed88
HoiLV <hoilv@evolableasia.vn>
2018-10-30 14:41:32 +0700

    update

--- app/Http/Controllers/User/SearchController.php
+++ app/Http/Controllers/User/SearchController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductsLanguages;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
     public function index(Request $request) {
-        var_dump($request['key']);
+        if (isset($request['key'])) {
+            $key = $request['key'];
+            $dataSearch = ProductsLanguages::User()
+                ->where(function ($q) use ($key) {
+                    $q->where('content', 'LIKE', "%$key%")
+                        ->orWhere('name', 'LIKE', "%$key%")
+                        ->orWhere('price', 'LIKE', "%$key%");
+                })->paginate(30);
+            return view('user.search.index', [
+                'key' => $key,
+                'dataSearch' => $dataSearch
+            ]);
+        }
+        
+        return redirect()->route('user_top');
     }
 }
--- resources/views/user/search/index.blade.php
+++ resources/views/user/search/index.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     <div class="row">
         <div class="col-md-12" style="padding-bottom: 15px;
-    border-bottom: 1px solid #fff3f3;">Trang Chủ > Sản Phẩm</div>
-        <h3 class="col-md-12" style="margin-bottom: 20px">Danh Sách Sản Phẩm</h3>
-        @foreach($data as $value)
+    border-bottom: 1px solid #fff3f3;">Trang Chủ > Tìm Kiếm</div>
+        <h5 class="col-md-12" style="margin-bottom: 20px"> Từ Khóa tìm kiếm : {{$key}}</h5>
+        
+        @foreach($dataSearch as $value)
             <div class="col-md-4 col-xs-12" style="margin-bottom: 15px">
                 <a class="title-name">{{$value['name']}}</a>
                 <div class="img-height">
@@ -19,5 +20,5 @@
             </div>
         @endforeach
     </div>
-    <div class="text-center">{{$data->links()}}</div>
+    <div class="text-center">{{$dataSearch->links()}}</div>
 @endsection
