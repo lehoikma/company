70a174ff98826836aae191f787adfbc03a6a5ab3
Hoi Le <hoile@Les-MacBook-Pro.local>
2020-06-18 23:56:59 +0700

    update design album image

--- resources/views/user/image/list_image.blade.php
+++ resources/views/user/image/list_image.blade.php
@@ -1,21 +1,122 @@
 @extends('user.layouts.app')
 @section('content')
-    <div class="mobile-content col-lg-9 col-md-9 col-sm-9 col-xs-12" style="padding: 0px">
-        <div class="col-md-12" style="padding-bottom: 15px;
-    border-bottom: 1px solid #fff3f3;">{{trans('messages.home')}} > Hình Ảnh</div>
-        <h3 class="col-md-12" style="margin-bottom: 10px; margin-top: 10px">Hình Ảnh</h3>
-        @foreach($categoryImage as $category)
-            <div class="image" style="text-align: center; margin-bottom: 10px;">
-                @foreach($listImage as $image)
-                    @if($category['id'] == $image['category_image_id'])
-                        <img src="upload/{{$image['image']}}" style="width: 100%; margin-bottom: 10px; margin-top:10px;"></br>
-                        @if(isset($image['description']))
-                            <i>{{$image['description']}}</i></br>
-                        @endif
-                    @endif
-                @endforeach
+    <section class="navigate-container">
+        <div class="container">
+            <div class="navigate">
+                <ol>
+                    <li>
+                        <a href=""><span>Trang chủ&nbsp;&gt;&nbsp;</span></a>
+                    </li>
+                    <li>
+                        <a href="" target="_self"><span>Thư viện tư liệu&nbsp; &gt;&nbsp; <span></span></span></a>
+                    <li>
+                        <a href="" target="_self"><span >Thư viện ảnh <span></span></span></a>
+                    </li>
+                </ol>
             </div>
-        @endforeach
-        <div class="col-md-12" style="border-bottom: 1px solid #eee;margin-bottom: 10px;"></div>
-    </div>
+        </div>
+    </section>
+
+    <section class="page-content page-gallery">
+        <div class="container">
+            <div class="box-title">
+                <h1>
+                    <a class="text" href="" target="_self">Thư viện ảnh</a></h1>
+            </div>
+            <div class="gallery-listing">
+                <div class="row" id="gall-list">
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                    <article class="col-lg-4 col-md-4 col-sm-4 col-xs-12 gall-item">
+                        <div class="item-box">
+                            <figure style="width: 100%; height: 213.514px;">
+                                <a href="">
+                                    <img src="upload/1537773999.jpeg"></a>
+                            </figure>
+                            <div class="gall-content">
+                                <a class="gall-title" href="" target="_self">
+                                    <p>Mavin liên kết đào tạo với Học viện Nông nghiệp Việt Nam</p>
+                                </a>
+                            </div>
+                        </div>
+                    </article>
+
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <a class="btn btn-loading" href="javascript:void(0)"><i class="fa fa-spinner"></i>Xem thêm</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
